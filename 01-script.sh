ba5d5370c6cddbab66692503404666568c982a05
SVR Kalluru <raghuk.vit@gmail.com>
2024-08-11 15:28:09 +0530

    This is a demo commit

--- 01-script.sh
+++ 01-script.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/env bash
-
 ls
+
+# THis is a comment line
+# Future reference of some notes
+# Reference for other people of some comments you wanna keep for them
+# If we want to not execute some commands as part of troubleshooting then also you can use these comments.
