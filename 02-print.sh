d099563c93e8db9218a5fced04375172fbb11230
SVR Kalluru <raghuk.vit@gmail.com>
2024-08-11 15:37:43 +0530

    Scripts

--- 02-print.sh
+++ 02-print.sh
@@ -19,5 +19,12 @@ echo -e Hello\\t\\tWorld
 # Syntax : echo -e \\e[COLmMESSAGE\\e[0m
 
 echo -e \\e[31m Hello in Red Color\\e[0m
+echo -e \\e[32m Hello in Green Color\\e[0m
+echo -e \\e[33m Hello in Yellow Color\\e[0m
+echo -e \\e[34m Hello in Blue Color\\e[0m
+echo -e \\e[35m Hello in Magenta Color\\e[0m
+echo -e \\e[36m Hello in Cyan Color\\e[0m
+
+
 
 
