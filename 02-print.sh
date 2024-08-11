fec0da13bbc35537a2cf7dcb6569fb58fd232454
SVR Kalluru <raghuk.vit@gmail.com>
2024-08-11 15:36:57 +0530

    Scripts

--- 02-print.sh
+++ 02-print.sh
@@ -7,4 +7,17 @@ echo -e Hello World\\nWelcome to shell training
 # (\\t)
 echo -e Hello\\t\\tWorld
 
+# (\\e)
+# Colors        Foreground Color ( Font )
+# Red             31
+# Green           32
+# Yellow          33
+# Blue            34
+# Magenta         35
+# Cyan            36
+
+# Syntax : echo -e \\e[COLmMESSAGE\\e[0m
+
+echo -e \\e[31m Hello in Red Color\\e[0m
+
 
